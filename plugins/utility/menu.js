391ef242d5a67d95d32d1ea1122aec0ebf7a1f28
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-19 19:58:45 +0800

    Update menu.js

--- plugins/utility/menu.js
+++ plugins/utility/menu.js
@@ -1,26 +1,42 @@
 export default {
-  name: "menu",
-  category: "utility",
-  command: ["menu"],
-  run: async (conn, m) => {
-  
-    let plugins = Object.values(conn.plugins);
-    
-    let grouped = {};
-    for (let plugin of plugins) {
-      if (!grouped[plugin.category]) grouped[plugin.category] = [];
-      grouped[plugin.category].push(plugin);
-    }
-    
-    let menu = "";
-    for (let [category, items] of Object.entries(grouped)) {
-      menu += `# ${category.toUpperCase()}\n`;
-      for (let item of items) {
-        menu += `> ${m.prefix}${item.name}\n`; 
+   name: "menu",
+   category: "utility",
+   command: ["menu"],
+   run: async (conn, m) => {
+
+      let plugins = Object.values(conn.plugins);
+
+      let grouped = {};
+      for (let plugin of plugins) {
+         if (!grouped[plugin.category]) grouped[plugin.category] = [];
+         grouped[plugin.category].push(plugin);
       }
-      menu += "\n";
-    }
-    
-    m.reply(menu);
-  }
+
+      let behh = m.args[0] ? m.args[0].toLowerCase() : null
+      let menu = "";
+      if (!behh) {
+      menu += `Hai 👋 ${m.pushname} ini daftar menu yg tersedia:\n\n` 
+         for (let category of Object.keys(grouped)) {
+            menu += `> ${category}\n`
+         }
+         menu += '\nKetik *.menu <kategori> untuk menampilkan menu *\n'
+         menu += 'Ketik *.menu all* untuk semua menu\n'
+      } else if (behh === "all") {
+         for (let [category, items] of Object.entries(grouped)) {
+            menu += `# ${category.toUpperCase()}\n`;
+            for (let item of items) {
+               menu += `> ${m.prefix}${item.name}\n`;
+            }
+            menu += "\n";
+         }
+      } else if (grouped[behh]) {
+         menu += `${behh.toUpperCase()} | MENU\n`
+         for (let item of grouped[behh]) {
+            menu += `> ${m.prefix}${item.name}`
+         }
+      } else {
+         return m.reply("Apaan itu bang ga tersedia di kategori 😅")
+      }
+      m.reply(menu);
+   }
 }
