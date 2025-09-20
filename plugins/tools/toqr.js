6171f878e5a51da2b5a29afb1ac1181b61e813e8
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-20 16:42:24 +0800

    Add files via upload

--- plugins/tools/toqr.js
+++ plugins/tools/toqr.js
@@ -6,10 +6,11 @@ export default {
       Func
    }) => {
       if (m.text) {
-         let apis = Func.fetchJson(`https://api-vgx.vercel.app/api/v2/tools/textToqr?text=${m.text}&ecLevel=H&margin=15&size=200&centerImageUrl=https%3A%2F%2Ffiles.catbox.moe%2Fimyxwz.jpg`)
+       //  let apis = Func.fetchJson(`https://api-vgx.vercel.app/api/v2/tools/textToqr?text=${m.text}&ecLevel=H&margin=15&size=200&centerImageUrl=https%3A%2F%2Ffiles.catbox.moe%2Fimyxwz.jpg`)
+       let tek = m.text // gabut
          m.reply({
             image: {
-               url: apis
+               url: `https://quickchart.io/qr?size=300&margin=2&text=${encodeURIComponent(tek)}`
             }
          })
       } else {
