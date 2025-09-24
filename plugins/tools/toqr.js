3898a8a5992daf63901edddc364cb0eb5f165e1f
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-24 09:26:05 +0800

    Add files via upload

--- plugins/tools/toqr.js
+++ plugins/tools/toqr.js
@@ -6,12 +6,12 @@ export default {
       Func
    }) => {
       if (m.text) {
-       //  let apis = Func.fetchJson(`https://api-vgx.vercel.app/api/v2/tools/textToqr?text=${m.text}&ecLevel=H&margin=15&size=200&centerImageUrl=https%3A%2F%2Ffiles.catbox.moe%2Fimyxwz.jpg`)
        let tek = m.text // gabut
          m.reply({
             image: {
                url: `https://quickchart.io/qr?size=300&margin=2&text=${encodeURIComponent(tek)}`
-            }
+            },
+            caption: "> Powered by Adzy"
          })
       } else {
        m.reply("Salah oi ketik text nya")
