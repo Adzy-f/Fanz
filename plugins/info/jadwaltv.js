7449972ec97c200d537400f0ebad97a327cd09fa
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-25 08:06:29 +0800

    dzy

---
+++ plugins/info/jadwaltv.js
@@ -0,0 +1,22 @@
+export default {
+   name: "jadwaltv",
+   category: "info",
+   command: ["jadwaltv"],
+   run: async (conn, m, {
+      Api
+   }) => {
+      try {
+         if (m.text) {
+            let api = Api.request("furina", "/search/jadwaltv", {
+               channel: m.text
+            })
+            let res = `*Jadwal tipi ${api?.channel}*\n> ${api?.schedule}`
+            m.reply(res)
+         } else {
+            m.reply("Nama tv?")
+         }
+      } catch (err) {
+         m.reply(err.message)
+      }
+   }
+}
