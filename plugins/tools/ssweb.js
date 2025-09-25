5eb6d29489783a74819f65cb257857123e597d59
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-25 19:58:04 +0800

    fix

--- plugins/tools/ssweb.js
+++ plugins/tools/ssweb.js
@@ -1,34 +1,28 @@
 export default {
-    name: "ssweb",
-    category: "tools",
-    command: ["ssweb"],
-    run: async (conn, m, { Func }) => {
-        if (!m.text) return m.reply("Masukin url");
-        let lnk = Func.isUrl(m.text);
-        try {
-            if (lnk) {
-                for (let i of lnk) {
-                    conn.sendMessage(m.chat, {
-                        image: {
-                            url: `https://api.mightyshare.io/v1/19EIFDUEL496RA3F/jpg?url=${Func.isUrl(i) ? i : `https://${i}`}`
-                        },
-                        caption: "- fnz"
-                    }, {
-                        quoted: m
-                    })
-                }
-            } else {
-                conn.sendMessage(m.chat, {
-                    image: {
-                        url: `https://api.mightyshare.io/v1/19EIFDUEL496RA3F/jpg?url=${m.text}`
-                    },
-                    caption: "- fnz"
-                }, {
-                    quoted: m
-                })
-            }
-        } catch (e) {
-            console.log(e);
-        }
-    }
+   name: "ssweb",
+   category: "tools",
+   command: ["ssweb"],
+   run: async (conn, m, {
+      Api
+   }) => {
+      if (!m.text) return m.reply("Ex: " + m.cmd + " urlmya");
+      await m.reply("Tunggu...")
+      try {
+         //let txt = m.text.split("|").map(t => t.trim())
+         let apis = await Api.createUrl("siput", "/api/tools/ssweb", {
+            url: m.text,
+            theme: "dark",
+            device: "desktop"
+         })
+         m.reply({
+            image: {
+               url: apis || "https://files.catbox.moe/x15h9q.jpg"
+            },
+            caption: "Sudah tak scrinsut [Mode desktop]"
+         })
+
+      } catch (e) {
+         console.log(e);
+      }
+   }
 }
