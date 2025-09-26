1f8b095787a2a2e058daaf57b6c2b62a979f3b40
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-27 04:15:33 +0800

    dzy

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import {
 const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default {
+   pkg: pkg,
    // seting bot dan owner
    numP: 962796121703, // no bot
    ownerN: ["962796171203"], // edit owner di sini
@@ -13,20 +14,22 @@ export default {
    botN: pkg.name,
    botF: pkg.nickname,
    version: pkg.version,
+   thumb: "https://files.catbox.moe/x15h9q.jpg",
+   url: "https://github.com/Adzy-f",
    // buat setiker pack
    pack: "Sticker by Fanz",
    auth: "Auhtor nya Adi",
    // perpesanan 
-   wait: 'Harap Tunggu sebentar',
-   owner: 'Fitur Ini Khusus Owner',
-   group: 'Fitur Ini Khusus Group',
-   admin: 'Fitur Ini Khusus Admin Group',
-   botAdmin: 'Bot Harus Menjadi Admin Group',
-   private: 'Fitur Ini Khusus Private',
+   wait: '+= Harap Tunggu sebentar',
+   owner: '+= Fitur Ini Khusus Owner',
+   group: '+= Fitur Ini Khusus Group',
+   admin: '+= Fitur Ini Khusus Admin Group',
+   botAdmin: '+= Bot Harus Menjadi Admin Group',
+   private: '+= Fitur Ini Khusus Private',
    //pair kode Luh 
    pairC: "FANZXSAD",
    // bebas
-   wm: "<!> Powered by Dzyy",
+   wm: "[#] Fanz WhatsApp Bot",
    // apikay ryhar
    ryhar: "y7r35v1jrzh4z9myfsj2ut"
 }
