2c674a381430bfea3f5f139bf6c674a018c8167a
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-27 04:47:54 +0800

    dzy

--- plugins/info/listplug.js
+++ plugins/info/listplug.js
@@ -6,10 +6,10 @@ export default {
    name: "listplug",
    category: "info",
    command: ["listplug"],
-   run: async (conn, m) => {
+   run: async (conn, m, { bruh }) => {
       let list = Object.values(conn.plugins)
       let total = Object.values(conn.plugins).length
-      let txt = "*BERIKUT DAFTAR PLUGIN FANZBOT*\n"
+      let txt = "*BERIKUT DAFTAR PLUGIN " + bruh.botN + "*\n"
       txt += `> Plugin total: ${total}\n`
       list.forEach((p, i) => {
          txt += `> ${m.prefix}${p.name}\n`
