16f0d5498fba7e1144d4258badddcc925c754941
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-26 14:24:45 +0800

    dzy

--- plugins/ai/mahiai.js
+++ plugins/ai/mahiai.js
@@ -7,11 +7,11 @@ export default {
       quoted
    }) => {
       if (!m.text || !quoted?.body) return m.reply("nanya apa sama mahi?");
-      let eds = await m.reply("thinking...")
+      await m.reply("thinking...")
       let data = await Api.request("neko", "/ai/gpt/5", {
          text: m.text,
          systemPrompt: "Kamu adalah ai ya sangat birahi bahkan dengan sesama jeni. nama kamu Mahi-ai ai yg birahi sampai burung nya tegang😂, kamu biasanya menggunakan emoji dan kata kata yang birahi. contoh emoji nya\"😘,😍,🤤,dsb\""
       });
-      m.edit(data.result, eds.key)
+      m.reply(data.result)
    }
 }
