2e81243f0242d7c3d941810bdb56aad1898f1c80
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-29 04:30:01 +0800

    dzy

--- plugins/info/tqto.js
+++ plugins/info/tqto.js
@@ -5,20 +5,13 @@ export default {
    run: async (conn, m, {
       Api
    }) => {
-
-      const teks = Object.entries(Api.listUrl())
-         .map(([name, {
-            baseURL
-         }]) => `${name}: ${baseURL}`)
-         .join("\n");
-
       let res = `Terimakasih pada:
-Pembuat base: github.com/AgusXzz 
-
-Thz juga buat:
-${teks}
-
-Bailyes: Whiskeysoket`
+> AgusXzz 
+> Purrbits
+> Allah
+> Ortu
+> Semua nya
+> Whiskeysoket`
 
       m.reply(res)
    }
