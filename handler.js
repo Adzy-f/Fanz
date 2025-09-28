c16a1966f8d5372a00fbfd1a1795ee9bda7ef5df
Adzy Mode Sad :( <maxzi8018@gmail.com>
2025-09-29 04:25:09 +0800

    dzy

--- handler.js
+++ handler.js
@@ -1,130 +1,119 @@
-import axios from 'axios';
-import {
-   jidNormalizedUser
-} from 'baileys';
-import util from 'util';
-import cp from 'child_process';
-import {
-   readFileSync
-} from 'fs';
+import axios from 'axios'
+import { jidNormalizedUser } from 'baileys'
+import util from 'util'
+import cp from 'child_process'
+import { readFileSync } from 'fs'
 
-import Api from './lib/api.js';
-import Func from './lib/function.js';
+import Api from '#lib/api.js'
+import Func from '#lib/function.js'
 const cfg = JSON.parse(readFileSync("./config.json", "utf-8"));
 
 export default async function Command(conn, m) {
-   let quoted = m.isQuoted ? m.quoted : m;
-   let downloadM = async (filename) => await conn.downloadMediaMessage(quoted, filename);
-   let isCommand = m.prefix && m.body.startsWith(m.prefix) || false;
-   const isOwner = m.fromMe || cfg.set.ownerN.includes(m.sender.split('@')[0]);
+  const quoted = m.isQuoted ? m.quoted : m
+  const downloadM = async (filename) => await conn.downloadMediaMessage(quoted, filename)
+  const isCommand = (m.prefix && m.body.startsWith(m.prefix)) || false
+  const isOwner = m.fromMe || cfg.set.ownerN.includes(m.sender.split('@')[0])
 
-   if (m.isBot) return;
-   if (!cfg.set.status && !isOwner) return;
-   const metadata = m.isGroup ? conn.chats[m.chat] || await conn.groupMetadata(m.chat).catch(_ => null) : {}
-   const groupAdmins = m.isGroup && (metadata.participants.reduce((memberAdmin, memberNow) => (memberNow.admin ? memberAdmin.push({
-      jid: memberNow.id?.endsWith('@s.whatsapp.net') ? memberNow.id : (memberNow.jid?.endsWith('@s.whatsapp.net') ? memberNow.jid : memberNow.phoneNumber),
-      admin: memberNow.admin
-   }) : [...memberAdmin]) && memberAdmin, []))
-   const isAdmin = m.isGroup && !!groupAdmins.find((member) => member.jid === m.sender)
-   const isBotAdmin = m.isGroup && !!groupAdmins.find((member) => member.jid === jidNormalizedUser(conn.user.id))
+  if (m.isBot) return
+  if (!cfg.set.status && !isOwner) return
 
-   const anunya = {
-      Api,
-      Func,
-      downloadM,
-      quoted,
-      metadata,
-      isOwner,
-      isAdmin,
-      isBotAdmin,
-      cfg
-   }
-   // === LOOPING PLUGINS ===
-   for (const plugin of Object.values(conn.plugins)) {
-      // EVENT LISTENER
-      if (typeof plugin.on === "function") {
-         try {
-            const handled = await plugin.on.call(conn, m, anunya);
-            if (handled) continue;
-         } catch (e) {
-            console.error(`[PLUGIN EVENT ERROR] ${plugin.name}`, e);
-         }
-      }
+  const metadata = m.isGroup ? conn.chats[m.chat] || (await conn.groupMetadata(m.chat).catch(() => null)) : {}
+  const isAdmin = m.isGroup && metadata.participants.find(u => conn.getJid(u.id) === m.sender).admin == 'admin' || false;
+  const isBotAdmin = m.isGroup && metadata.participants.find(u => conn.getJid(u.id) === jidNormalizedUser(conn.user.id)).admin == 'admin' || false;
 
-      // COMMAND HANDLER
-      if (isCommand) {
-         const command = m.command?.toLowerCase();
-         const isCmd = plugin?.command?.includes(command) || (plugin?.alias && plugin.alias.includes(command));
+  const ctx = {
+    Api,
+    Func,
+    downloadM,
+    quoted,
+    metadata,
+    isOwner,
+    isAdmin,
+    isBotAdmin,
+    cfg
+  }
 
-         try {
-            if (isCmd) {
-               if (plugin.settings?.owner && !isOwner) {
-                  m.reply(cfg.mess.owner);
-                  continue;
-               }
-               if (plugin.settings?.private && m.isGroup) {
-                  m.reply(cfg.mess.private);
-                  continue;
-               }
-               if (plugin.settings?.group && !m.isGroup) {
-                  m.reply(cfg.mess.group);
-                  continue;
-               }
-               if (plugin.settings?.admin && !isAdmin) {
-                  m.reply(cfg.mess.admin);
-                  continue;
-               }
-               if (plugin.settings?.botAdmin && !isBotAdmin) {
-                  m.reply(cfg.mess.botAdmin);
-                  continue;
-               }
-               if (plugin.settings?.loading) {
-                  m.reply(cfg.mess.wait);
-               }
-
-               plugin.run(conn, m, anunya);
-            }
-         } catch (e) {
-            console.error(`[PLUGIN ERROR] ${plugin.name}`, e);
-            await m.reply("Terjadi error saat menjalankan command.");
-         }
-      }
-   };
-
-   if ([">", "=>"].some(a => m.body?.toLowerCase().startsWith(a)) && isOwner) {
-      let evalCmd = ""
+  for (const plugin of Object.values(plugins)) {
+    if (typeof plugin.on === 'function') {
       try {
-         evalCmd = /await/i.test(m.text) ?
-            eval("(async() => { " + m.text + " })()") :
-            eval(m.text)
+        const handled = await plugin.on.call(conn, m, ctx)
+        if (handled) continue
       } catch (e) {
-         evalCmd = e
+        console.error(`[PLUGIN EVENT ERROR] ${plugin.name}`, e)
       }
-      new Promise(async (resolve, reject) => {
-            try {
-               resolve(evalCmd)
-            } catch (err) {
-               reject(err)
-            }
-         })
-         ?.then((res) => m.reply(util.format(res)))
-         ?.catch((err) => m.reply(util.format(err)))
-   }
+    }
 
-   if (m.body?.startsWith('$') && isOwner) {
-      const exec = util.promisify(cp.exec).bind(cp)
-      let o
+    if (isCommand) {
+      const command = m.command?.toLowerCase()
+      const isCmd =
+        plugin?.command?.includes(command) ||
+        (plugin?.alias && plugin.alias.includes(command))
+
       try {
-         o = await exec(m.text)
+        if (isCmd) {
+          if (plugin.settings?.owner && !isOwner) {
+            m.reply(cfg.mess.owner)
+            continue
+          }
+          if (plugin.settings?.private && m.isGroup) {
+            m.reply(cfg.mess.private)
+            continue
+          }
+          if (plugin.settings?.group && !m.isGroup) {
+            m.reply(cfg.mess.group)
+            continue
+          }
+          if (plugin.settings?.admin && !isAdmin) {
+            m.reply(cfg.mess.admin)
+            continue
+          }
+          if (plugin.settings?.botAdmin && !isBotAdmin) {
+            m.reply(cfg.mess.botAdmin)
+            continue
+          }
+          if (plugin.settings?.loading) m.reply(cfg.mess.wait)
+
+          plugin.run(conn, m, ctx)
+        }
       } catch (e) {
-         o = e
-      } finally {
-         let {
-            stdout,
-            stderr
-         } = o
-         if (stdout.trim()) m.reply(stdout)
-         if (stderr.trim()) m.reply(stderr)
+        console.error(`[PLUGIN ERROR] ${plugin.name}`, e)
+        await m.reply('Terjadi error saat menjalankan command.')
       }
-   }
-};
+    }
+  }
+
+  if (['>', '=>'].some((a) => m.body?.toLowerCase().startsWith(a)) && isOwner) {
+    let evalCmd = ''
+    try {
+      evalCmd = /await/i.test(m.text)
+        ? eval(`(async() => { ${m.text} })()`)
+        : eval(m.text)
+    } catch (e) {
+      evalCmd = e
+    }
+
+    new Promise((resolve, reject) => {
+      try {
+        resolve(evalCmd)
+      } catch (err) {
+        reject(err)
+      }
+    })
+      ?.then((res) => m.reply(util.format(res)))
+      ?.catch((err) => m.reply(util.format(err)))
+  }
+
+  if (m.body?.startsWith('$') && isOwner) {
+    const exec = util.promisify(cp.exec).bind(cp)
+    let o
+    try {
+      o = await exec(m.text)
+    } catch (e) {
+      o = e
+    } finally {
+      const { stdout, stderr } = o
+      if (stdout.trim()) m.reply(stdout)
+      if (stderr.trim()) m.reply(stderr)
+    }
+  }
+}
